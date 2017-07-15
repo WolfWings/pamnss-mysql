281b401851b6ef9b4c6c6110b2d00d126a508278
Wolf <wolfwings@gmail.com>
2017-07-15 14:57:34 -0500

    Initial NSS skeleton

--- Makefile
+++ Makefile
@@ -13,7 +13,9 @@ CFLAGS=$(MY_CFLAGS) $(shell mysql_config --cflags) -I. -std=gnu99
 LDLIBS=$(MY_LDLIBS) $(shell mysql_config --libs)
 LDFLAGS=$(MY_LDFLAGS)
 
-default: tests/parser_coverage
+default: libnss_mysql2017.so.2
+
+tests: tests/parser_coverage
 
 tests/parser_coverage: parser.o config.o tests/parser_coverage.c
 
@@ -21,5 +23,12 @@ parser.c: config.h
 
 config.c: config.h
 
+mysql.c: mysql.h
+
+nss.c: nss.h
+
+libnss_mysql2017.so.2: nss.c config.c parser.c mysql.c
+	gcc -fPIC -shared -o libnss_mysql2017.so.2 -Wl,-soname,libnss_mysql2017.so.2 nss.c
+
 clean:
-	$(RM) *.o *.gcda *.gcno *.gcov tests/*_coverage
+	$(RM) *.o *.gcda *.gcno *.gcov tests/*_coverage *.so.*
---
+++ nss.c
@@ -0,0 +1,80 @@
+#include <nss.h>
+#include <pwd.h>
+#include <grp.h>
+#include <syslog.h>
+
+/* /etc/passwd coverage */
+
+enum nss_status _nss_mysql2017_setpwent (void) { // FIXME
+	syslog(LOG_WARNING, "[PAMNSS-MySQL-2017] - setpwent called");
+	return NSS_STATUS_NOTFOUND;
+}
+
+enum nss_status _nss_mysql2017_getpwent_r (void) { // FIXME
+	syslog(LOG_WARNING, "[PAMNSS-MySQL-2017] - getpwent_r called");
+	return NSS_STATUS_NOTFOUND;
+}
+
+enum nss_status _nss_mysql2017_endpwent (void) { // FIXME
+	syslog(LOG_WARNING, "[PAMNSS-MySQL-2017] - endpwent called");
+	return NSS_STATUS_NOTFOUND;
+}
+
+enum nss_status _nss_mysql2017_getpwuid_r (void) { // FIXME
+	syslog(LOG_WARNING, "[PAMNSS-MySQL-2017] - getpwuid_r called");
+	return NSS_STATUS_NOTFOUND;
+}
+
+enum nss_status _nss_mysql2017_getpwnam_r (void) { // FIXME
+	syslog(LOG_WARNING, "[PAMNSS-MySQL-2017] - getpwnam_r called");
+	return NSS_STATUS_NOTFOUND;
+}
+
+/* /etc/group coverage */
+
+enum nss_status _nss_mysql2017_setgrent (void) { // FIXME
+	syslog(LOG_WARNING, "[PAMNSS-MySQL-2017] - setgrent called");
+	return NSS_STATUS_NOTFOUND;
+}
+
+enum nss_status _nss_mysql2017_getgrent_r (void) { // FIXME
+	syslog(LOG_WARNING, "[PAMNSS-MySQL-2017] - getgrent_r called");
+	return NSS_STATUS_NOTFOUND;
+}
+
+enum nss_status _nss_mysql2017_endgrent (void) { // FIXME
+	syslog(LOG_WARNING, "[PAMNSS-MySQL-2017] - endgrent called");
+	return NSS_STATUS_NOTFOUND;
+}
+
+enum nss_status _nss_mysql2017_getgrgid_r (void) { // FIXME
+	syslog(LOG_WARNING, "[PAMNSS-MySQL-2017] - getgrgid_r called");
+	return NSS_STATUS_NOTFOUND;
+}
+
+enum nss_status _nss_mysql2017_getgrnam_r (void) { // FIXME
+	syslog(LOG_WARNING, "[PAMNSS-MySQL-2017] - getgrnam_r called");
+	return NSS_STATUS_NOTFOUND;
+}
+
+/* /etc/shadow coverage */
+
+enum nss_status _nss_mysql2017_setspent (void) { // FIXME
+	syslog(LOG_WARNING, "[PAMNSS-MySQL-2017] - setspent called");
+	return NSS_STATUS_NOTFOUND;
+}
+
+enum nss_status _nss_mysql2017_getspent_r (void) { // FIXME
+	syslog(LOG_WARNING, "[PAMNSS-MySQL-2017] - getspent_r called");
+	return NSS_STATUS_NOTFOUND;
+}
+
+enum nss_status _nss_mysql2017_endspent (void) { // FIXME
+	syslog(LOG_WARNING, "[PAMNSS-MySQL-2017] - endspent called");
+	return NSS_STATUS_NOTFOUND;
+}
+
+enum nss_status _nss_mysql2017_getspnam_r (void) { // FIXME
+	syslog(LOG_WARNING, "[PAMNSS-MySQL-2017] - getspnam_r called");
+	return NSS_STATUS_NOTFOUND;
+}
